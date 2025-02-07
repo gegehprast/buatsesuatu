4434fe5c9159abfd5bd7f76f1f99d0b43605a925
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-02-07 17:20:37 +0700

    feat(cropemall): add useMovableWithMouse hook for mouse-based drag-and-drop functionality

---
+++ apps/cropemall/src/hooks/useMovableWithMouse.tsx
@@ -0,0 +1,76 @@
+import { Vector } from '@cropemall/math'
+import React, { useEffect, useRef, useState } from 'react'
+
+type UseMovableWithMouse<T extends HTMLElement> = [
+    React.MutableRefObject<T | null>,
+]
+
+const useMovableWithMouse = <
+    T extends HTMLElement,
+>(): UseMovableWithMouse<T> => {
+    const elementRef = useRef<T>(null)
+    const startPos = useRef(new Vector(0, 0))
+    const currPos = useRef(new Vector(0, 0))
+
+    const [isDragging, setIsDragging] = useState(false)
+    const [position, setPosition] = useState(new Vector(0, 0))
+
+    useEffect(() => {
+        const element = elementRef.current
+
+        if (!element) {
+            return
+        }
+
+        function handleMouseDown(e: MouseEvent) {
+            e.preventDefault()
+            startPos.current = new Vector(e.clientX, e.clientY)
+            setIsDragging(true)
+        }
+
+        element.addEventListener('mousedown', handleMouseDown)
+
+        return () => {
+            element.removeEventListener('mousedown', handleMouseDown)
+        }
+    }, [])
+
+    useEffect(() => {
+        function handleMouseMove(e: MouseEvent) {
+            if (!isDragging) {
+                return
+            }
+
+            currPos.current = new Vector(e.clientX, e.clientY)
+
+            const force = currPos.current.sub(startPos.current)
+
+            setPosition((pos) => pos.add(force))
+
+            startPos.current = currPos.current
+        }
+
+        function handleMouseUp() {
+            setIsDragging(false)
+        }
+
+        window.document.addEventListener('mousemove', handleMouseMove)
+        window.document.addEventListener('mouseup', handleMouseUp)
+
+        return () => {
+            window.document.removeEventListener('mousemove', handleMouseMove)
+            window.document.removeEventListener('mouseup', handleMouseUp)
+        }
+    }, [isDragging])
+
+    useEffect(() => {
+        if (elementRef.current) {
+            elementRef.current.style.cursor = 'move'
+            elementRef.current.style.transform = `translate(${position.x}px, ${position.y}px)`
+        }
+    }, [position])
+
+    return [elementRef]
+}
+
+export default useMovableWithMouse
