785e7648275913e14c487b1309375c2349954157
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-02-07 22:10:53 +0700

    feat(cropemall): enhance Editor component with dynamic image sizing and integrate Cropper

--- apps/cropemall/src/components/Editor.tsx
+++ apps/cropemall/src/components/Editor.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import background from '@/assets/background.png'
 import useMovableWithMouse from '@/hooks/useMovableWithMouse'
+import Cropper from './Cropper'
 
 interface CropperProps {
     img: string
@@ -8,28 +9,54 @@ interface CropperProps {
 }
 
 const Editor: React.FC<CropperProps> = ({ img, alt }) => {
-    const [image] = useMovableWithMouse<HTMLImageElement>()
-    const [cropper] = useMovableWithMouse<HTMLDivElement>()
+    const container = useRef<HTMLDivElement>(null)
+    const [image, position] = useMovableWithMouse<HTMLImageElement>()
+    const [size, setSize] = useState({ width: 0, height: 0 })
+
+    const calculateImageSize = useCallback(() => {
+        if (!container.current || !image.current) {
+            return
+        }
+
+        // get the container size
+        const { height } = container.current.getBoundingClientRect()
+
+        // calculate the image size based on the container height
+        const ratio = image.current.naturalWidth / image.current.naturalHeight
+        const imgWidth = height * ratio
+        const imgHeight = height
+
+        setSize({ width: imgWidth, height: imgHeight })
+    }, [image])
+
+    useEffect(() => {
+        calculateImageSize()
+    }, [calculateImageSize])
 
     return (
-        <div className="flex flex-col aspect-video">
+        <div className="flex flex-col aspect-video p-2">
             <div
+                ref={container}
                 className="relative overflow-hidden border border-collapse border-mf-500 aspect-video"
                 style={{ backgroundImage: `url(${background})` }}
                 onDragOver={(e) => e.preventDefault()}
             >
                 <img
+                    ref={image}
                     src={img}
                     alt={alt}
-                    className="absolute object-contain h-full select-none"
-                    ref={image}
+                    className="absolute select-none w-full h-full"
+                    style={{
+                        width: `${size.width}px`,
+                        height: `${size.height}px`,
+                    }}
+                    onLoad={calculateImageSize}
                 />
 
+                <div className="absolute w-full h-full bg-mf-900/25 pointer-events-none"></div>
+
                 {/* crop square */}
-                <div
-                    ref={cropper}
-                    className="absolute w-40 h-40 border border-mf-500 select-none"
-                />
+                <Cropper image={img} size={size} position={position} />
             </div>
         </div>
     )
