b2d70141007de670c6d493c83348361450ab523b
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-02-10 01:50:21 +0700

    refactor(cropemall): reorganize button layout and remove zoom instructions in App component

--- apps/cropemall/src/App.tsx
+++ apps/cropemall/src/App.tsx
@@ -35,37 +35,32 @@ function App() {
             </main>
 
             {files.length > 0 && (
-                <div className="absolute p-2 rounded bg-mf-200 bottom-4 right-8">
-                    {/* control + wheel to zoom info */}
-                    <p className="text-black">ctrl + wheel to zoom</p>
+                <div className="absolute p-2 flex flex-row gap-2 rounded bg-mf-200 bottom-4 right-8">
+                    <button
+                        onClick={crop}
+                        title="Crop all"
+                        className="p-2 px-4 text-white rounded-md bg-mf-500 hover:bg-mf-600 transition-colors duration-300 ease-in-out cursor-pointer"
+                    >
+                        cropemall
+                    </button>
 
-                    <div className="flex flex-row gap-2 rounded bg-mf-200 bottom-4 right-8 mt-2">
-                        <button
-                            onClick={crop}
-                            title="Crop all"
-                            className="p-2 px-4 text-white rounded-md bg-mf-500 hover:bg-mf-600 transition-colors duration-300 ease-in-out cursor-pointer"
-                        >
-                            cropemall
-                        </button>
+                    <button
+                        onClick={reset}
+                        title="Reset all"
+                        className="p-2 px-4 text-white rounded-md bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out cursor-pointer"
+                    >
+                        Reset all
+                    </button>
 
-                        <button
-                            onClick={reset}
-                            title="Reset all"
-                            className="p-2 px-4 text-white rounded-md bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out cursor-pointer"
-                        >
-                            Reset all
-                        </button>
-
-                        <button
-                            onClick={() => {
-                                setFiles([])
-                            }}
-                            title="Reset all"
-                            className="p-2 px-4 text-white rounded-md bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out cursor-pointer"
-                        >
-                            Start over
-                        </button>
-                    </div>
+                    <button
+                        onClick={() => {
+                            setFiles([])
+                        }}
+                        title="Reset all"
+                        className="p-2 px-4 text-white rounded-md bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out cursor-pointer"
+                    >
+                        Start over
+                    </button>
                 </div>
             )}
         </div>
