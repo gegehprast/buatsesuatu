2b65fe2ff07ca75aca697391155a9b50ced34851
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-02-10 01:36:04 +0700

    feat(cropemall): update UI to include zoom instructions and reorganize button layout in App component

--- apps/cropemall/src/App.tsx
+++ apps/cropemall/src/App.tsx
@@ -35,33 +35,38 @@ function App() {
             </main>
 
             {files.length > 0 && (
-                <nav className="absolute flex flex-row gap-2 p-2 rounded bg-mf-200 bottom-4 right-8">
-                    <button
-                        onClick={crop}
-                        title="Crop all"
-                        className="p-2 px-4 text-white rounded-md bg-mf-500 hover:bg-mf-600 transition-colors duration-300 ease-in-out cursor-pointer"
-                    >
-                        cropemall
-                    </button>
+                <div className="absolute p-2 rounded bg-mf-200 bottom-4 right-8">
+                    {/* control + wheel to zoom info */}
+                    <p className="text-black">ctrl + wheel to zoom</p>
 
-                    <button
-                        onClick={reset}
-                        title="Reset all"
-                        className="p-2 px-4 text-white rounded-md bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out cursor-pointer"
-                    >
-                        Reset all
-                    </button>
+                    <div className="flex flex-row gap-2 rounded bg-mf-200 bottom-4 right-8 mt-2">
+                        <button
+                            onClick={crop}
+                            title="Crop all"
+                            className="p-2 px-4 text-white rounded-md bg-mf-500 hover:bg-mf-600 transition-colors duration-300 ease-in-out cursor-pointer"
+                        >
+                            cropemall
+                        </button>
 
-                    <button
-                        onClick={() => {
-                            setFiles([])
-                        }}
-                        title="Reset all"
-                        className="p-2 px-4 text-white rounded-md bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out cursor-pointer"
-                    >
-                        Start over
-                    </button>
-                </nav>
+                        <button
+                            onClick={reset}
+                            title="Reset all"
+                            className="p-2 px-4 text-white rounded-md bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out cursor-pointer"
+                        >
+                            Reset all
+                        </button>
+
+                        <button
+                            onClick={() => {
+                                setFiles([])
+                            }}
+                            title="Reset all"
+                            className="p-2 px-4 text-white rounded-md bg-gray-500 hover:bg-gray-600 transition-colors duration-300 ease-in-out cursor-pointer"
+                        >
+                            Start over
+                        </button>
+                    </div>
+                </div>
             )}
         </div>
     )
