3876721126df83320fe7128a260a6c1df4d705b4
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-02-12 02:50:11 +0700

    feat(cropemall): implement CropperRotateHandler and custom hook for rotation functionality

--- apps/cropemall/src/components/CropperRotateHandler.tsx
+++ apps/cropemall/src/components/CropperRotateHandler.tsx
@@ -1,24 +1,50 @@
 import { useCropper } from '@/hooks/useCropper'
+import useCropperRotateHandler from '@/hooks/useCropperRotateHandler'
 import React from 'react'
 
 const CropperRotateHandler: React.FC = () => {
-    const {
-        setImgRotation,
-    } = useCropper()
-
-    const rotate = (add: number) => {
-        setImgRotation((prev) => prev + add)
-    }
+    const { imgRotation } = useCropper()
+    const [containerRef, barRefs, center] =
+        useCropperRotateHandler()
 
     return (
-        <div className="absolute bottom-0 left-0 space-x-2 p-2 bg-mf-300/50 w-full text-white z-10">
-            <button className="w-1/4 bg-mf-500" onClick={() => rotate(-0.05)}>
-                -1
-            </button>
-            <button className="w-1/4 bg-mf-500" onClick={() => rotate(0.05)}>
-                +1
-            </button>
+        <div className="absolute bottom-0 left-0 space-x-2 p-2 w-full flex items-center text-white z-10">
+            <div
+                ref={containerRef}
+                className="relative w-[200px] mx-auto h-4 flex items-center overflow-hidden"
+            >
+                {barRefs.map((barRef, i) => {
+                    return (
+                        <Bar
+                            ref={barRef}
+                            key={i}
+                            className={i === center ? 'h-4 ' : ''}
+                        />
+                    )
+                })}
+
+                {/* center bar */}
+                <div className="absolute w-0.5 h-4 bg-mf-400 left-1/2 -translate-x-1/2" />
+            </div>
+
+            {/* rotation lebel */}
+            <div className="absolute top-0 left-0 text-center">
+                {imgRotation / (Math.PI / 180)}° | {imgRotation}rad
+            </div>
         </div>
+    )
+}
+
+interface BarProps extends React.HTMLAttributes<HTMLDivElement> {
+    ref: React.RefObject<HTMLDivElement | null>
+}
+
+const Bar: React.FC<BarProps> = ({ ref, ...props }) => {
+    return (
+        <div
+            ref={ref}
+            className={`absolute w-0.5 h-3 bg-gray-100 ${props.className}`}
+        />
     )
 }
 
---
+++ apps/cropemall/src/hooks/useCropperRotateHandler.tsx
@@ -0,0 +1,104 @@
+import { Vector } from '@cropemall/math'
+import { createRef, useEffect, useRef } from 'react'
+import { useCropper } from './useCropper'
+
+const barCount = 61
+const center = Math.floor(barCount / 2) - 1
+const bars = Array.from({ length: barCount }, (_, i) => {
+    const centerX = 100
+    const offset = (i - center) * 10
+    const width = 1
+    const pos = new Vector(centerX + offset - width, 0)
+
+    return pos
+})
+const maxAngle = 180
+const maxAngleRadian = maxAngle * (Math.PI / 180)
+
+const offsetForceToRadians = (force: Vector) => {
+    return force.x * (Math.PI / 180)
+}
+
+const radiansToOffsetForce = (radian: number) => {
+    return new Vector(radian / (Math.PI / 180), 0)
+}
+
+const useCropperRotateHandler = () => {
+    const { imgRotation, setImgRotation } = useCropper()
+    const containerRef = useRef<HTMLDivElement | null>(null)
+    const startPos = useRef<Vector>(new Vector(0, 0))
+    const currPos = useRef<Vector>(new Vector(0, 0))
+    const barRefs = bars.map(() => createRef<HTMLDivElement>())
+
+    useEffect(() => {
+        const container = containerRef.current
+
+        if (!container) return
+
+        function handleMouseDown(e: MouseEvent) {
+            e.preventDefault()
+            e.stopPropagation()
+
+            startPos.current = new Vector(e.clientX, e.clientY)
+
+            function handleMouseMove(e: MouseEvent) {
+                e.preventDefault()
+                e.stopPropagation()
+
+                currPos.current = new Vector(e.clientX, e.clientY)
+
+                const force = currPos.current.sub(startPos.current).normalize()
+
+                setImgRotation((prev) => {
+                    const currentForce = radiansToOffsetForce(prev)
+                    const newForce = currentForce.add(new Vector(force.x, 0))
+                    const radian = offsetForceToRadians(newForce)
+
+                    if (radian > maxAngleRadian) return maxAngleRadian
+
+                    if (radian < -maxAngleRadian) return -maxAngleRadian
+
+                    return radian
+                })
+
+                startPos.current = currPos.current
+            }
+
+            function handleMouseUp(e: MouseEvent) {
+                e.preventDefault()
+                e.stopPropagation()
+
+                document.body.removeEventListener('mousemove', handleMouseMove)
+            }
+
+            document.body.addEventListener('mousemove', handleMouseMove)
+            document.body.addEventListener('mouseup', handleMouseUp, {
+                once: true,
+            })
+        }
+
+        container.addEventListener('mousedown', handleMouseDown)
+
+        return () => {
+            container.removeEventListener('mousedown', handleMouseDown)
+        }
+    }, [barRefs, setImgRotation])
+
+    useEffect(() => {
+        barRefs.forEach((barRef, i) => {
+            const bar = barRef.current
+
+            if (!bar) return
+
+            const pos = bars[i]
+            const offsetForce = radiansToOffsetForce(imgRotation)
+            const newPos = pos.add(offsetForce)
+
+            bar.style.transform = `translateX(${newPos.x}px)`
+        })
+    }, [barRefs, imgRotation])
+
+    return [containerRef, barRefs, center] as const
+}
+
+export default useCropperRotateHandler
