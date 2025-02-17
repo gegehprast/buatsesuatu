f7256263b9e536ee0c4383228a2dc64d963a124c
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-02-17 22:14:08 +0700

    feat(cropemall): add canonical link and dynamic URLs for Open Graph and Twitter meta tags

--- apps/cropemall/vite.config.ts
+++ apps/cropemall/vite.config.ts
@@ -1,14 +1,25 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return {
+        plugins: [
+            react(),
+            tailwindcss(),
+            Sitemap({
+                hostname: env.VITE_APP_URL,
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
         },
-    },
+    }
 })
