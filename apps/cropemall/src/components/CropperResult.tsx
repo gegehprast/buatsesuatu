edf07741dae92dbdef81003fbec9876decbf4556
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-02-18 01:17:17 +0700

    feat(cropemall): add close button to CropperResult for clearing the result

--- apps/cropemall/src/components/CropperResult.tsx
+++ apps/cropemall/src/components/CropperResult.tsx
@@ -1,17 +1,26 @@
 import { useCropper } from '@/hooks/useCropper'
+import { LucideX } from 'lucide-react'
 
 const CropperResult = () => {
-    const { result } = useCropper()
+    const { result, setResult } = useCropper()
 
     return (
         <>
             {result && (
-                <div className="flex justify-center items-center gap-2 w-full h-[500px] p-4">
+                <div className="relative flex justify-center items-center gap-2 w-full h-[500px] p-4">
                     <img
                         src={result}
                         alt="Result"
                         className="h-full object-contain outline-1 outline-mf-500"
                     />
+
+                    {/* absolute close button */}
+                    <button
+                        onClick={() => setResult(undefined)}
+                        className="absolute top-4 right-4 p-2 rounded-full transition-colors duration-300 ease-in-out text-gray-700 cursor-pointer"
+                    >
+                        <LucideX size={24} />
+                    </button>
                 </div>
             )}
         </>
